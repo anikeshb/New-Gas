7f3a4e5e704718193de420a55752cd74fc82e9c4
anikesh <saurabhu@prdxn.com>
2018-05-22 13:19:21 +0530

    header scroll completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-/* Author: 
 
-*/
+window.onscroll = function() {scroll()};
 
+var navbar = document.getElementsByTagName("header");
+var sticky = navbar[0].offsetTop;
 
+function scroll() {
+console.log(window.pageYOffset);
+  if (window.pageYOffset == 0) {
+    navbar[0].classList.remove("sticky");
+  } else {
+    navbar[0].classList.add("sticky");
+  }
+}
 
-
-
-
-
-
-
-
-
+var btn=document.getElementById("down");
+var content=document.getElementById("scrolldown");
+btn.addEventListener('click',function () { scrollDown(); });
 
 
 
