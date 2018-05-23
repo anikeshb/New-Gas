db07ed0365e39e594b5f8545803bf3f3adea2e99
anikesh <saurabhu@prdxn.com>
2018-05-23 11:49:45 +0530

    Hamburger dropdown alternate opening fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,65 @@
+ $(document).ready(function() {
 
-// ------------------scroll-------------------------
+	// ------------------scroll-------------------------
 
-window.onscroll = function() {scroll()};
-
-var navbar = document.getElementsByTagName("header");
-var sticky = navbar[0].offsetTop;
-
-function scroll() {
-  if (window.pageYOffset == 0) {
-    navbar[0].classList.remove("sticky");
-  } else {
-    navbar[0].classList.add("sticky");
-  }
-}
-// -----------------------------------------------------
-
-// ------------------Dropdown------------------------
-
-
-var btn=document.getElementById("down");
-var content=document.getElementById("scrolldown");
-btn.addEventListener('click',function () { scrollDown(); });
-function show() {
-	dropdown[0].classList.toggle('open');
-}
-
-var dropdown= document.getElementsByClassName('dropdown');
-dropdown[0].addEventListener('click',function() { show(); });
-
-// ---------------------------------------------------
-
-
-// ----------------------hamburger-------------------
-
-	var flag=0;
-	var bar=document.getElementsByClassName('hamburger');
-  var menu=document.getElementsByTagName('nav');
-  bar[0].addEventListener("click",function(){mobileMenu();});
-	
-	function mobileMenu ( ) {
-		if(flag==0)
-		{
-			menu[0].classList.add("mobile");
-			bar[0].classList.add("cross");
-			flag++;
-		}	else {
-			flag=0;
-			menu[0].classList.remove("mobile");
-			bar[0].classList.remove("cross");
-		} 
+	function scroll() {
+	  if (window.pageYOffset == 0) {
+	    navbar[0].classList.remove("sticky");
+	  } else {
+	    navbar[0].classList.add("sticky");
+	  }
 	}
 
+	var navbar = document.getElementsByTagName("header");
+	var sticky = navbar[0].offsetTop;
+	window.onscroll = function() {scroll()};
 
-// ---------------------------------------------
+	// -----------------------------------------------------
+
+	// ----------------------hamburger-------------------
+
+	function mobileMenu ( ) {
+		if(checkClass[1]=='open'){
+			dropdown[0].classList.toggle('open');
+			body.classList.toggle('hide');
+			menu[0].classList.toggle("mobile");
+			bar[0].classList.toggle("cross");
+		} else {
+			body.classList.toggle('hide');
+			menu[0].classList.toggle("mobile");
+			bar[0].classList.toggle("cross");
+		}
+	}
+	
+	var dropdown= document.getElementsByClassName('dropdown');
+	var checkClass=dropdown[0].classList;
+
+	var body=document.querySelector('html');
+	var checkClass2=body.classList;
+	
+	var bar=document.getElementsByClassName('hamburger');
+  var menu=document.getElementsByTagName('nav');
+
+  bar[0].addEventListener("click",function(){mobileMenu();});
+	
+	// ---------------------------------------------
+
+	// ------------------Dropdown------------------------
+
+	function show() {
+		if(checkClass2[0]=='hide') {
+			body.classList.toggle('hide');
+			menu[0].classList.toggle("mobile");
+			bar[0].classList.toggle("cross");
+			dropdown[0].classList.toggle('open');
+		}else	dropdown[0].classList.toggle('open');
+	}
+
+	dropdown[0].addEventListener('click',function() { show(); });
+	
+	// ---------------------------------------------------
+
+});
 
 
 
