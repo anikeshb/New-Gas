b4a8d1bf5554c91a76314e2c85e633a82a04de55
anikesh <saurabhu@prdxn.com>
2018-05-29 15:47:08 +0530

    Fonts issue fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,12 @@
 		$(window).on('load', function() {
 
 			if($(window).width() > 1024) {
-				$('.quote').draggable(); 
-			}	
+					$('.quote').draggable();
+			}	else if($(window).width() < 1024 || $(window).width() > 980) {
+				$('.quote').on('touchstart touchmove touchend', function() {
+					$('.quote').draggable(); 
+				});
+			}
 
 	    $( "#accordion" ).accordion({
 	      collapsible: true,
